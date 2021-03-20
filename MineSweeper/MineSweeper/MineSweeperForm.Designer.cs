422e574806ecb272bf4a3922964f0c50ff6c85b2
Sandor Csanak <sandor.csanak@borsodchem.eu>
2021-03-20 14:35:45 +0100

    Project

---
+++ MineSweeper/MineSweeper/MineSweeperForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace MineSweeper
+{
+    partial class MineSweeperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MineSweeperForm));
+            this.tileGrid = new MineSweeper.MineSweeperForm.TileGrid();
+            this.flagCounter_lbl = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.Game_menuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGame_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.NewGame_Beginner_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGame_Intermediate_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGame_Expert_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.Information_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit_menuStripOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameButton_btn = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameButton_btn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tileGrid
+            // 
+            this.tileGrid.Location = new System.Drawing.Point(10, 50);
+            this.tileGrid.Name = "tileGrid";
+            this.tileGrid.Size = new System.Drawing.Size(260, 199);
+            this.tileGrid.TabIndex = 0;
+            // 
+            // flagCounter_lbl
+            // 
+            this.flagCounter_lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flagCounter_lbl.AutoSize = true;
+            this.flagCounter_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flagCounter_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.flagCounter_lbl.Location = new System.Drawing.Point(221, 8);
+            this.flagCounter_lbl.Name = "flagCounter_lbl";
+            this.flagCounter_lbl.Size = new System.Drawing.Size(2, 33);
+            this.flagCounter_lbl.TabIndex = 2;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Game_menuStrip});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // Game_menuStrip
+            // 
+            this.Game_menuStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewGame_menuStripOption,
+            this.menuStripSeparator1,
+            this.NewGame_Beginner_menuStripOption,
+            this.NewGame_Intermediate_menuStripOption,
+            this.NewGame_Expert_menuStripOption,
+            this.menuStripSeparator2,
+            this.Information_menuStripOption,
+            this.Exit_menuStripOption});
+            this.Game_menuStrip.Name = "Game_menuStrip";
+            this.Game_menuStrip.Size = new System.Drawing.Size(45, 20);
+            this.Game_menuStrip.Text = "Játék";
+            // 
+            // NewGame_menuStripOption
+            // 
+            this.NewGame_menuStripOption.Name = "NewGame_menuStripOption";
+            this.NewGame_menuStripOption.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.NewGame_menuStripOption.Size = new System.Drawing.Size(180, 22);
+            this.NewGame_menuStripOption.Text = "Új játék";
+            this.NewGame_menuStripOption.Click += new System.EventHandler(this.NewGame_menuStripOption_Click);
+            // 
+            // menuStripSeparator1
+            // 
+            this.menuStripSeparator1.Name = "menuStripSeparator1";
+            this.menuStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // NewGame_Beginner_menuStripOption
+            // 
+            this.NewGame_Beginner_menuStripOption.Name = "NewGame_Beginner_menuStripOption";
+            this.NewGame_Beginner_menuStripOption.Size = new System.Drawing.Size(180, 22);
+            this.NewGame_Beginner_menuStripOption.Tag = "Beginner";
+            this.NewGame_Beginner_menuStripOption.Text = "Kezdő";
+            this.NewGame_Beginner_menuStripOption.Click += new System.EventHandler(this.MenuStip_DifficultyChanged);
+            // 
+            // NewGame_Intermediate_menuStripOption
+            // 
+            this.NewGame_Intermediate_menuStripOption.Name = "NewGame_Intermediate_menuStripOption";
+            this.NewGame_Intermediate_menuStripOption.Size = new System.Drawing.Size(180, 22);
+            this.NewGame_Intermediate_menuStripOption.Tag = "Intermediate";
+            this.NewGame_Intermediate_menuStripOption.Text = "Középhaladó";
+            this.NewGame_Intermediate_menuStripOption.Click += new System.EventHandler(this.MenuStip_DifficultyChanged);
+            // 
+            // NewGame_Expert_menuStripOption
+            // 
+            this.NewGame_Expert_menuStripOption.Name = "NewGame_Expert_menuStripOption";
+            this.NewGame_Expert_menuStripOption.Size = new System.Drawing.Size(180, 22);
+            this.NewGame_Expert_menuStripOption.Tag = "Expert";
+            this.NewGame_Expert_menuStripOption.Text = "Haladó";
+            this.NewGame_Expert_menuStripOption.Click += new System.EventHandler(this.MenuStip_DifficultyChanged);
+            // 
+            // menuStripSeparator2
+            // 
+            this.menuStripSeparator2.Name = "menuStripSeparator2";
+            this.menuStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // Exit_menuStripOption
+            // 
+            this.Exit_menuStripOption.Name = "Exit_menuStripOption";
+            this.Exit_menuStripOption.Size = new System.Drawing.Size(180, 22);
+            this.Exit_menuStripOption.Tag = "Exit";
+            this.Exit_menuStripOption.Text = "Kilépés";
+            this.Exit_menuStripOption.Click += new System.EventHandler(this.Exit_menuStripOption_Click);
+            // 
+            // gameButton_btn
+            // 
+            this.gameButton_btn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.gameButton_btn.BackgroundImage = global::MineSweeper.Properties.Resources.game_button;
+            this.gameButton_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.gameButton_btn.Location = new System.Drawing.Point(125, 8);
+            this.gameButton_btn.Name = "gameButton_btn";
+            this.gameButton_btn.Size = new System.Drawing.Size(35, 35);
+            this.gameButton_btn.TabIndex = 1;
+            this.gameButton_btn.TabStop = false;
+            this.gameButton_btn.Click += new System.EventHandler(this.LoadGame_Click);
+            // 
+            // MineSweeperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.flagCounter_lbl);
+            this.Controls.Add(this.gameButton_btn);
+            this.Controls.Add(this.tileGrid);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "MineSweeperForm";
+            this.Text = "Aknakereső";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameButton_btn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TileGrid tileGrid;
+
+        private System.Windows.Forms.PictureBox gameButton_btn;
+        private System.Windows.Forms.Label flagCounter_lbl;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem Game_menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem NewGame_menuStripOption;
+        private System.Windows.Forms.ToolStripSeparator menuStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem NewGame_Beginner_menuStripOption;
+        private System.Windows.Forms.ToolStripMenuItem NewGame_Intermediate_menuStripOption;
+        private System.Windows.Forms.ToolStripMenuItem NewGame_Expert_menuStripOption;
+        private System.Windows.Forms.ToolStripSeparator menuStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem Information_menuStripOption;
+        private System.Windows.Forms.ToolStripMenuItem Exit_menuStripOption;
+    }
+}
+
---
+++ MineSweeper/MineSweeper/MineSweeperForm.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+using MineSweeper.Properties;
+
+namespace MineSweeper
+{
+    public partial class MineSweeperForm : Form
+    {
+        private enum Difficulty { Expert, Intermediate, Beginner }
+
+        private Difficulty difficulty;
+
+        public MineSweeperForm()
+        {
+            InitializeComponent();
+
+            this.LoadGame_Click(null, null);
+            this.tileGrid.TileFlagStatusChange += this.TileFlagStatusChenged;
+        }
+
+        private void TileFlagStatusChenged (object sender, TileGrid.TileFlagStatusChangedEventArgs e)
+        {
+            this.flagCounter_lbl.Text = e.Flags.ToString();
+            this.flagCounter_lbl.ForeColor = e.LabelColor;
+        }
+
+        private class TileGrid : Panel
+        {
+            private static readonly HashSet<Tile> gridSearchBlacklist = new HashSet<Tile>();
+            private static readonly Random rnd = new Random();
+
+            private Size gridSize;
+
+            private int mineCnt;
+            private int flagCnt;
+            private bool minesGenerated;
+
+            internal event EventHandler<TileFlagStatusChangedEventArgs> TileFlagStatusChange = delegate { };
+
+            private Tile this[Point point] => (Tile)Controls[$"Tile_{point.X}_{point.Y}"];
+
+            private class Tile : PictureBox
+            {
+                internal const int length = 26;
+
+                private static readonly int[][] adjacentCoords =
+                {
+                    new [] {-1, -1}, new [] {0, -1}, new [] {1, -1}, new [] {1, 0}, new [] {1, 1}, new [] {0, 1}, new [] {-1, 1}, new [] {-1, 0}
+                };
+
+                private bool flagged;
+
+                internal Tile(int x, int y)
+                {
+                    this.Name = $"Tile_{x}_{y}";
+                    this.Location = new Point(x * length, y * length);
+                    this.GridPosition = new Point(x, y);
+                    this.Size = new Size(length, length);
+                    this.Image = Resources.tile;
+                    this.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+
+                internal Tile[] AdjacentTiles { get; private set; }
+
+                internal Point GridPosition { get; }
+
+                internal bool Opened { get; private set; }
+                internal bool Mined { get; private set; }
+                internal bool Flagged
+                {
+                    get => this.flagged;
+                    set
+                    {
+                        this.flagged = value;
+                        this.Image = value ? Resources.flag : Resources.tile;
+                    }
+                }
+
+                private int AdjacentMines => this.AdjacentTiles.Count(tile => tile.Mined);
+
+                internal void SetAdjacentTiles()
+                {
+                    TileGrid tileGrid = (TileGrid)this.Parent;
+
+                    List<Tile> adjacentTiles = new List<Tile>(8);
+
+                    foreach (int[] adjacentCoord in adjacentCoords)
+                    {
+                        Tile tile = tileGrid[new Point(this.GridPosition.X + adjacentCoord[0], this.GridPosition.Y + adjacentCoord[1])];
+                        if (tile != null)
+                        {
+                            adjacentTiles.Add(tile);
+                        }
+                    }
+
+                    this.AdjacentTiles = adjacentTiles.ToArray();
+                }
+
+                internal void TestAdjacentTiles()
+                {
+                    if (this.flagged || gridSearchBlacklist.Contains(this))
+                    {
+                        return;
+                    }
+
+                    gridSearchBlacklist.Add(this);
+
+                    if (this.AdjacentMines == 0)
+                    {
+                        foreach (Tile tile in this.AdjacentTiles)
+                            tile.TestAdjacentTiles();
+                    }
+
+                    this.Open();
+                }
+
+                internal void Mine()
+                {
+                    this.Mined = true;
+                }
+
+                private void Open()
+                {
+                    this.Opened = true;
+                    this.Image = (Image)Resources.ResourceManager.GetObject($"cell_{this.AdjacentMines}");
+                }
+            }
+
+            internal class TileFlagStatusChangedEventArgs : EventArgs
+            {
+                internal int Flags { get; }
+
+                internal Color LabelColor { get; }
+
+                internal TileFlagStatusChangedEventArgs(int flags, Color labelColor)
+                {
+                    this.Flags = flags;
+                    this.LabelColor = labelColor;
+                }
+            }
+
+            private void Tile_MouseDown(object sender, MouseEventArgs e)
+            {
+                Tile tile = sender as Tile;
+
+                if (!tile.Opened)
+                {
+                    switch (e.Button)
+                    {
+                        case MouseButtons.Left when !tile.Flagged:
+                            if (!this.minesGenerated)
+                                this.GenerateMines(tile);
+
+                            if (tile.Mined)
+                            {
+                                this.DisableTiles(true);
+                            }
+                            else
+                            {
+                                tile.TestAdjacentTiles();
+                                gridSearchBlacklist.Clear();
+                            }
+                            break;
+                        case MouseButtons.Right when this.flagCnt > 0:
+                            if (tile.Flagged)
+                            {
+                                tile.Flagged = false;
+
+                                this.flagCnt++;
+                            }
+                            else
+                            {
+                                tile.Flagged = true;
+
+                                this.flagCnt--;
+                            }
+                            TileFlagStatusChange(this, new TileFlagStatusChangedEventArgs(this.flagCnt, this.flagCnt < this.flagCnt * 0.25 ? Color.Red : Color.Black));
+                            break;
+                    }
+                }
+
+                this.CheckForWin();
+            }
+
+            internal void LoadGrid(Size gridSize, int mines)
+            {
+                this.Controls.Clear();
+
+                this.minesGenerated = false;
+
+                this.gridSize = gridSize;
+                this.mineCnt = flagCnt = mines;
+                this.Size = new Size(gridSize.Width * Tile.length, gridSize.Height * Tile.length);
+
+                for(int x = 0; x < gridSize.Width; x++)
+                    for(int y = 0; y < gridSize.Height; y++)
+                    {
+                        Tile tile = new Tile(x, y);
+                        tile.MouseDown += Tile_MouseDown;
+
+                        Controls.Add(tile);
+                    }
+
+                foreach (Tile tile in this.Controls)
+                    tile.SetAdjacentTiles();
+            }
+
+            private void GenerateMines(Tile safeTile)
+            {
+                int safeTileCount = safeTile.AdjacentTiles.Length + 1;
+
+                Point[] usedPositions = new Point[this.mineCnt + safeTileCount];
+                usedPositions[0] = safeTile.GridPosition;
+
+                for (int i = 1; i < safeTileCount; i++)
+                    usedPositions[i] = safeTile.AdjacentTiles[i - 1].GridPosition;
+
+                for (int i = safeTileCount; i < usedPositions.Length; i++)
+                {
+                    Point point = new Point(rnd.Next(this.gridSize.Width), rnd.Next(this.gridSize.Height));
+
+                    if (!usedPositions.Contains(point))
+                    {
+                        this[point].Mine();
+
+                        usedPositions[i] = point;
+                    }
+                    else
+                        i--;
+
+                    this.minesGenerated = true;
+                }
+            }
+
+            private void DisableTiles(bool isGameOver)
+            {
+                foreach (Tile tile in this.Controls)
+                {
+                    tile.MouseDown -= this.Tile_MouseDown;
+
+                    if (isGameOver)
+                    {
+                        tile.Image = !tile.Opened && tile.Mined && !tile.Flagged ? Resources.mine : tile.Flagged && !tile.Mined ? Resources.false_flag : tile.Image;
+                    }
+                }
+            }
+
+            private void CheckForWin()
+            {
+                if (this.flagCnt != 0 || this.Controls.OfType<Tile>().Count(tile => tile.Opened || tile.Flagged) != this.gridSize.Width * this.gridSize.Height)
+                    return;
+
+                MessageBox.Show("Ügyes, megoldottad a játékot!", "Nyertél", MessageBoxButtons.OK);
+
+                this.DisableTiles(false);
+            }
+        }
+
+        private void LoadGame_Click(object sender, EventArgs e)
+        {
+            int x, y, mines;
+
+            switch (difficulty)
+            {
+                case Difficulty.Beginner:
+                    x = y = 9;
+                    mines = 10;
+                    break;
+                case Difficulty.Intermediate:
+                    x = y = 16;
+                    mines = 40;
+                    break;
+                case Difficulty.Expert:
+                    x = 32;
+                    y = 24;
+                    mines = 99;
+                    break;
+                default:
+                    throw new InvalidOperationException("Nem implementált nehézségi szint.");
+            }
+
+            this.tileGrid.LoadGrid(new Size(x, y), mines);
+            this.MaximumSize = this.MinimumSize = new Size(this.tileGrid.Width + 36, this.tileGrid.Height + 98);
+            this.flagCounter_lbl.Text = mines.ToString();
+            this.flagCounter_lbl.ForeColor = Color.Black;
+        }
+
+        private void NewGame_menuStripOption_Click(object sender, EventArgs e)
+        {
+            this.LoadGame_Click(null, null);
+        }
+
+        private void Exit_menuStripOption_Click(object sender, EventArgs e)
+        {
+            var dialogResult = MessageBox.Show("Biztos ki akarsz lépni?", "Játék bezárása", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void MenuStip_DifficultyChanged(object sender, EventArgs e)
+        {
+            this.difficulty = (Difficulty)Enum.Parse(typeof(Difficulty), (string)((ToolStripMenuItem)sender).Tag);
+            this.LoadGame_Click(null, null);
+        }
+    }
+}
